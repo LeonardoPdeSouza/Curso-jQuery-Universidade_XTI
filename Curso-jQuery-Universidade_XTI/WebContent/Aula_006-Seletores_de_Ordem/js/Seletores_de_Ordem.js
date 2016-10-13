351e34fd8888072683d5d96e03bc319834dbd016
leona <leona@Leonardo>
2016-10-13 16:38:33 -0300

    Seletores_de_Ordem

---
+++ Curso-jQuery-Universidade_XTI/WebContent/Aula_006-Seletores_de_Ordem/js/Seletores_de_Ordem.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	//$("tbody tr:first") // primeiro elemento
+	//.css("background-color", "#ccc");
+	//$("tbody tr:not(tbody tr:first)") // todos menos o primeiro elemento
+	//.css("background-color", "#ccc");
+	//$("tbody tr:even") // todos os elementos pares
+	//.css("background-color", "#ccc");
+	//$("tbody tr:odd") // todos os elementos impares
+	//.css("background-color", "#ccc");
+	//$("tbody tr:eq(3)") // elemento especifico
+	//.css("background-color", "#ccc");
+	//$("tbody tr:gt(5)") // todos os elementos maiores que
+	//.css("background-color", "#ccc");
+	$("tbody tr:gt(1)") // todos os elementos menores que
+	.css("background-color", "#ccc");
+	
+});
