552831bf41ffce91bf61e27a0b3a0eeb5bded0a7
leona <leona@Leonardo>
2016-10-13 11:46:08 -0300

    Seletores_Simples

---
+++ Curso-jQuery-Universidade_XTI/WebContent/Aula_004-Seletores_Simples/js/Seletores_Simples.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+		//$("*").css("color", "#0000FF"); // Todos os elementos
+		$("#titulo").css("color", "green"); // elementos com id titulo
+		$("a").css("color", "red"); // Todos os com tag a
+		$(".outro").css("color", "black"); // elementos com a classe outro
+		$("h1,a").css("background-color", "yellow"); // elementos com as tag h1 e a
+	});
