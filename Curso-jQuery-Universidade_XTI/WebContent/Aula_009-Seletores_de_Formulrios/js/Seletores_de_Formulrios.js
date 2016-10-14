5a9c5e6665c8693d488a9223a8942422742b76c5
leona <leona@Leonardo>
2016-10-13 23:13:31 -0300

    Seletores_de_Formulrios

---
+++ Curso-jQuery-Universidade_XTI/WebContent/Aula_009-Seletores_de_Formulrios/js/Seletores_de_Formulrios.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	//$(":input").css("border", "3px solid #ff0000"); // seleciona todos os elementos input
+	
+	//$(":button").css("border", "3px solid #ff0000"); // seleciona todos elementos button
+	
+	$(":button").click(function(){ // clicar em algum elemento button chama a function
+		//$(":text").css("border", "3px solid #ff0000"); // aplica css a todos os elementos text
+		//$(":password").css("border", "3px solid #00ff00"); // aplica css a todos os elementos password
+		//$(":radio").parent().css("border", "3px solid #0000ff"); // //aplica css a todos os elementos pai de radio
+		//$(":checkbox").parent().css("border", "3px solid #f00000"); // aplica css a todos os elementos pai de checkbox
+		//$("select").css("border", "3px solid #0f0000"); //aplica css a todos os elementos select
+		//$("textarea").css("border", "3px solid #00f000"); //aplica css a todos os elementos textarea
+		//$(":submit").css("border", "3px solid #000f00"); //aplica css a todos os elementos submit
+		//$(":reset").css("border", "3px solid #0000f0"); //aplica css a todos os elementos reset
+		//$(":file").css("border", "3px solid #00000f"); //aplica css a todos os elementos button
+		//$(":enabled").css("border", "3px solid #f0000f"); // aplica css a todos os elementos enabled
+		//$(":disabled").css("border", "3px solid #ff0000"); // aplica css a todos os elementos disable
+		//$(":checked").parent().css("border", "3px solid #ff0000"); //aplica css a todos os elementos pai de checked
+		$(":selected").css("border", "3px solid #0f0000"); //aplica css a todos os elementos selected
+		
+	})
+	
+});
